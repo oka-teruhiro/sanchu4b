4e38855bf1ac2c1ff8bd433240d5f4329d5297b3
oka-teruhiro <oka331@gmail.com>
2022-12-26 22:07:11 +0900

    2:2022.12.26 22:06 ホームページ修正1

---
+++ android/app/src/main/kotlin/com/chan/sanchu4b/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chan.sanchu4b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sanchu42/android/app/src/main/kotlin/com/example/sanchu42/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sanchu42
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
